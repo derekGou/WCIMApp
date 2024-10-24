ae5a46304831276aa5ac05ca44594b5852a5856e
Derek Gou <derekgou19@gmail.com>
2024-10-24 19:25:21 -0400

    Keys patch

--- src/App.tsx
+++ src/App.tsx
@@ -78,10 +78,11 @@ useEffect(()=>{
 export default function App() {
   // See Demo API key Terms and Conditions
   // https://developer.mappedin.com/v6/demo-keys-and-maps/
+  console.log(import.meta.env.VITE_MY_KEY)
   const { isLoading, error, mapData } = useMapData({
     mapId: '66f7ff6142d0ac000b79d8db',
-    key: process.env.KEY,
-    secret: process.env.SECRET,
+    key: import.meta.env.VITE_MY_KEY,
+    secret: import.meta.env.VITE_MY_SECRET,
   });
 
   if (isLoading) {
